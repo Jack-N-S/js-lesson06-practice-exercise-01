f2bc33ee17bb3bd99529ad4e93f50208a887ed51
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-08-06 22:44:25 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var shoutIt = function (name) {
+    var shout = `Hello ${name}, nice to meet you!`.toUpperCase();
+    return shout;
+}
+
+console.log(shoutIt("Jack"));
+console.log(shoutIt("Lily"));
+console.log(shoutIt("Henry"));
